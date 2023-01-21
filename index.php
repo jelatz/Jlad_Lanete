984f7e3a4eba916120f33662c34c7334e30e4271
jelatz <jladaudrey@gmail.com>
2023-01-22 01:21:40 +0800

    about

--- index.php
+++ index.php
@@ -11,7 +11,76 @@
 </head>
 
 <body>
-    <section style="background-image: url(_assets/images/back.gif); background-position:center center; background-size:contain;  background-repeat:no-repeat; background-color:black; width:100%; height:100%;">
+    <section id="home" style="background-image: url(_assets/images/back.gif); background-position:center center; background-size:contain;  background-repeat:no-repeat; background-color:black; width:100%; height:100%;">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-2">
+                    <nav class="navbar navbar-expand-lg" style="width: 10rem;">
+                        <div class="justify-content-between" style="width: 10rem;">
+                            <a href="index.php" class="navbar-brand"><img src="_assets/images/logo(1).png" alt="logo" width="200" height="50" class="m-2"></a>
+                            <button class="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                <ul class="navbar-nav mx-auto flex-column text-white gap-5 mt-5">
+                                    <a href="#about" class="text-decoration-none text-white">
+                                        <li class="nav-item fw-bold">About</li>
+                                    </a>
+                                    <a href="#portfolio" class="text-decoration-none text-white">
+                                        <li class="nav-item fw-bold">Portfolio</li>
+                                    </a>
+                                    <a href="#services" class="text-decoration-none text-white">
+                                        <li class="nav-item fw-bold">Services</li>
+                                    </a>
+                                    <a href="#contact" class="text-decoration-none text-white">
+                                        <li class="nav-item fw-bold">Contact</li>
+                                    </a>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ABOUT -->
+    <section id="about" class="w-100 h-100 bg-dark d-flex align-items-center">
+        <div class="container">
+            <div class="moving-object"></div>
+            <div class="row justify-content-center text-center">
+                <div class="col">
+                    <img src="_assets/images/profile.jpg" alt="profile" width="300" height="300" class="rounded-circle">
+                </div>
+                <div class="col">
+                    <p class="text-white">Hello Everyone! I am Jay, a dedicated graduating student and currently an intern of Accenture Philippines. Everything started way back 2018 when my client asked me to help him out with his website. I did learn some things online and got interested in web development. I supported myself to school and I am where I am now because of my hard work. 
+
+                    Fast forward, I am now a freelance web developer and a virtual assistant at the same time. Eager to explore new things specially in the field of technology. 
+
+                </p>
+                <figure class="text-center">
+                <blockquote class="blockquote">
+                <p class="text-white">
+                The beautiful thing about learning is that nobody can take it away from you.
+                </p>
+                </blockquote>
+                <figcaption class="blockquote-footer">
+                    <cite>B.B. King</cite>
+                </figcaption>
+                </figure>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- PORTFOLIO -->
+    <section id="portfolio" class="w-100 h-100">
+        <h1>hello</h1>
+    </section>
+    <!-- SERVICES -->
+    <section id="services" class="w-100 h-100">
+        <h1>hello</h1>
+    </section>
+    <!-- CONTACT -->
+    <section id="contact" class="w-100 h-100 bg-dark">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-lg-2">
@@ -23,10 +92,21 @@
                             </button>
                             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                                 <ul class="navbar-nav mx-auto flex-column text-white gap-4 mt-5">
-                                    <a href="#" class="text-decoration-none text-white"><li class="nav-item fw-bold">About</li></a>
-                                    <a href="#" class="text-decoration-none text-white"><li class="nav-item fw-bold">Portfolio</li></a>
-                                    <a href="#" class="text-decoration-none text-white"><li class="nav-item fw-bold">Services</li></a>
-                                    <a href="#" class="text-decoration-none text-white"><li class="nav-item fw-bold">Contact</li></a>
+                                    <a href="#home" class="text-decoration-none text-white">
+                                        <li class="nav-item fw-bold">Home</li>
+                                    </a>
+                                    <a href="#about" class="text-decoration-none text-white">
+                                        <li class="nav-item fw-bold">About</li>
+                                    </a>
+                                    <a href="#portfolio" class="text-decoration-none text-white">
+                                        <li class="nav-item fw-bold">Portfolio</li>
+                                    </a>
+                                    <a href="#services" class="text-decoration-none text-white">
+                                        <li class="nav-item fw-bold">Services</li>
+                                    </a>
+                                    <a href="#contact" class="text-decoration-none text-white">
+                                        <li class="nav-item fw-bold">Contact</li>
+                                    </a>
                                 </ul>
                             </div>
                         </div>
@@ -35,10 +115,8 @@
             </div>
         </div>
     </section>
-    <section id="about" class="w-100 h-100">
-
-    </section>
     <script src="../_assets/js/bootstrap.bundle.js"></script>
+
 </body>
 
 </html>
