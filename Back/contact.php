35e7bbc9f7a885afc0937804abc268fec9c4dbea
jelatz <jladaudrey@gmail.com>
2023-01-26 18:08:38 +0800

    contact me

--- Back/contact.php
+++ Back/contact.php
@@ -5,16 +5,15 @@ $email = $_POST['email'];
 $to = "lanetejlad@gmail.com";
 $subject = "Web Services Inquiry";
 $message = $_POST['inquiry'];
-$headers = "From: $email";
+$headers = "From: $email\r\n";
 
 mail($to, $subject, $message, $headers);
 echo 'hay naku';
 if(mail($to,$subject,$message,$headers)){
     echo 'Message Sent!';
-    header('Location:../index.html?sent="Message Sent Successfully!"');
-    exit();
-}else{
-    header('Location:../index.html');
-    exit();
 }
+// else{
+//     header('Location:../index.php');
+//     exit();
+// }
 ?>
