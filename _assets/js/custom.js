12428b95bf43d3da31617e0fa69dcbd71adde184
jelatz <jladaudrey@gmail.com>
2024-01-01 01:24:19 +0800

    fixing the slider

--- _assets/js/custom.js
+++ _assets/js/custom.js
@@ -1,37 +1,54 @@
-// TYPEWRITER EFFECT
-const text2 = document.getElementById("positions");
-const texts = [
-  " ",
-  "Web Developer",
-  "Virtual Assistant",
-  "Software Engineer Associate",
-];
-let index = 0;
-let charIndex = 0;
-let isDeleting = false;
+$(document).ready(function () {
+  // TYPEWRITER EFFECT
+  const text2 = document.getElementById("positions");
+  const texts = [
+    " ",
+    "Web Developer",
+    "Virtual Assistant",
+    "Software Engineer Associate",
+  ];
+  let index = 0;
+  let charIndex = 0;
+  let isDeleting = false;
 
-function typeEffect() {
-  const text = texts[index];
-  if (isDeleting) {
-    text2.textContent = text.substring(0, charIndex - 1);
-    charIndex--;
-    if (charIndex === 0) {
-      isDeleting = false;
-      index = (index + 1) % texts.length;
+  function typeEffect() {
+    const text = texts[index];
+    if (isDeleting) {
+      text2.textContent = text.substring(0, charIndex - 1);
+      charIndex--;
+      if (charIndex === 0) {
+        isDeleting = false;
+        index = (index + 1) % texts.length;
+      }
+    } else {
+      text2.textContent = text.substring(0, charIndex + 1);
+      charIndex++;
+      if (charIndex === text.length) {
+        isDeleting = true;
+        setTimeout(typeEffect, 1000);
+        return;
+      }
     }
-  } else {
-    text2.textContent = text.substring(0, charIndex + 1);
-    charIndex++;
-    if (charIndex === text.length) {
-      isDeleting = true;
-      setTimeout(typeEffect, 1000);
-      return;
-    }
+    setTimeout(typeEffect, 100);
   }
-  setTimeout(typeEffect, 100);
-}
 
-typeEffect();
+  typeEffect();
 
-// AOS
-AOS.init();
+  // AOS INITIALIZATION
+  AOS.init();
+
+  // SLICK
+
+  $(".skill").slick({
+    slidesToShow: 5,
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 3500,
+    arrows: false,
+    dots: false,
+    waitForAnimate: true,
+    useCSS: false,
+    variableWidth: true,
+    infinite: true,
+  });
+});
