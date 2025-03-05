ad658b3baed37e48705fb235ca65e05f13bb65cb
Jelatz <lanetejlad@gmail.com>
2025-03-05 22:45:02 +0800

    added vue to upgrade my portfolio

---
+++ Jlad_Lanete/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
+import Header from '@/components/Header.vue'
+</script>
+
+<template>
+  <Header />
+  <!-- <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+      </nav>
+    </div>
+  </header> -->
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ Jlad_Lanete/src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <header class="w-full justify-between items-center flex p-10">
+        <div>
+            <a href="mailto:lanetejlad@gmail.com"
+                class="relative me-11 after:content-['|'] after:absolute after:-right-6 hover:text-[#00ADB5] text-lg">lanetejlad@gmail.com</a>
+            <a href="tel:+639760275569" class="hover:text-[#00ADB5] text-lg">09760275569</a>
+        </div>
+        <nav class="flex items-center space-x-10">
+            <a href="https://www.linkedin.com/in/jladlanete/" target="_blank" class="text-white hover:text-[#00ADB5]">
+                <Linkedin />
+            </a>
+            <a href="https://github.com/jelatz" target="_blank" class="text-white hover:text-[#00ADB5]">
+                <Github />
+            </a>
+        </nav>
+    </header>
+</template>
+
+
+<script setup>
+import Github from '@/components/svg/github.vue';
+import Linkedin from '@/components/svg/Linkedin.vue';
+</script>
+
+<style lang="scss" scoped></style>
