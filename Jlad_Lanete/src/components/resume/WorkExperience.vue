bde4f29216034c5e947079ee116cbef7b46017a0
Jelatz <lanetejlad@gmail.com>
2025-03-19 21:34:51 +0800

    commit

---
+++ Jlad_Lanete/src/components/resume/WorkExperience.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="border-b-2 p-5">
+        <h4
+            class="text-left text-2xl z-50 tracking-[5px] relative font-bold before:content-[''] before:w-8 before:h-8 before:rounded-full before:bg-slate-300 before:absolute before:top-1/2 before:-translate-y-1/2 before:-left-2 before:z-0">
+            <span class="relative z-10">W</span>ORK EXPERIENCE
+        </h4>
+        <div class="flex mt-3 flex-col">
+            <p>Detail-oriented and innovative IT professional with extensive experience
+                in web development, software development, and IT administration.
+                Proficient in front-end and back-end technologies, eager to contribute to
+                a dynamic team to deliver high-quality technological solutions.</p>
+        </div>
+    </div>
+</template>
+
+
+<script setup>
+
+</script>
+
+<style scoped></style>
