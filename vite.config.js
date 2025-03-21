60647f89c8e0b581bec97a53411bee66e14c2464
Jelatz <lanetejlad@gmail.com>
2025-03-21 22:14:32 +0800

    added new dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
-    tailwindcss(),
+    tailwindcss()
   ],
   build: {
     outDir: 'dist', // Output folder after build
