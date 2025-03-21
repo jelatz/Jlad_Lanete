7cb7e6da72406575885aaf7ef33d48566a0839ab
Jelatz <lanetejlad@gmail.com>
2025-03-21 23:13:21 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,16 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import tailwindcss from '@tailwindcss/vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),    tailwindcss()
+    vueDevTools()
   ],
-  
   build: {
-    outDir: 'dist', // Output folder after build
+    outDir: 'dist',
   },
   resolve: {
     alias: {
